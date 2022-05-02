9aaeaa45eaf7f3a6171157fca09d34e73614dc77
xuan13hao <xuan13hao@gmail.com>
2022-05-02 09:53:49 -0700

    update code

---
+++ stocksell.py
@@ -0,0 +1,57 @@
+# find MaxSubArray of subArray where mid element sits in
+def findMaxCrossingSubArray(A, low, mid, high):
+    maxLeft = 0
+    leftSum = float('-inf')
+    sum = 0
+    for i in range(mid, -1, -1):
+        sum = sum + A[i]
+        if sum > leftSum:
+            leftSum = sum
+            maxLeft = i
+
+    maxRight = 0
+    rightSum = float('-inf')
+    sum = 0
+    for i in range(mid+1, high + 1, 1):
+        sum = sum + A[i]
+        if sum > rightSum:
+            rightSum = sum
+            maxRight = i
+
+    return (maxLeft, maxRight, leftSum + rightSum)
+
+
+# recursive method to find maxSubArray
+def findMaxSubArray(A, low, high):
+    print("low: ", low, "high: ", high)
+    # recursive stop contidion
+    if low == high:
+        return(low, high, A[low])
+    else:
+        mid = (low + high)//2
+        # subArray sits in left subarray
+        # or right subarray
+        # or cross them
+        # find maxium of them as the final result
+        leftLow, leftHigh, leftSum = findMaxSubArray(A, low, mid)
+        righLow, righHigh, rightSum = findMaxSubArray(A, mid + 1, high)
+        crossLow, crossHigh, crossSum = findMaxCrossingSubArray(
+            A, low, mid, high)
+        if leftSum >= rightSum and leftSum >= crossSum:
+            return(leftLow, leftHigh, leftSum)
+        elif rightSum >= leftSum and rightSum >= crossSum:
+            return(righLow, righHigh, rightSum)
+        else:
+            return(crossLow, crossHigh, crossSum)
+
+
+if __name__ == '__main__':
+    A = [200, 500, 1000, 700, 30, 400, 900, 400, 50]
+    B = []
+    for n in range(1, len(A)):
+       B.append(A[n] - A[n-1])
+    print(B)
+    maxSubArray = findMaxSubArray(B, 0, len(B) - 1)
+    print("LeftIndex: ", maxSubArray[0])
+    print("RightIndex: ", maxSubArray[1])
+    print("maxSubArraySum: ", maxSubArray[2])
